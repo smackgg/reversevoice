bc9b2177a40cb3879edfa48dafc7161bf8d59bde
smackgg <smackgg@gmail.com>
2019-11-12 14:49:48 +0800

    winston add color

--- server/src/util/logger.ts
+++ server/src/util/logger.ts
@@ -4,6 +4,10 @@ const options: winston.LoggerOptions = {
     transports: [
         new winston.transports.Console({
             level: process.env.NODE_ENV === 'production' ? 'error' : 'debug',
+          format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.simple(),
+          ),
         }),
         new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
     ],
