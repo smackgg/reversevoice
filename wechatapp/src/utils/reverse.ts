3277b368560cd17d5739c567974f3a990d1f5178
smackgg <smackgg@gmail.com>
2019-11-15 15:59:33 +0800

    reverse common util

--- wechatapp/src/utils/reverse.ts
+++ wechatapp/src/utils/reverse.ts
@@ -12,51 +12,63 @@ export type LocalFileInfo = {
   path: string,
   index: number,
   reverseFilePath: string,
-  file: File,
-  reverseFile: File,
+  file?: File,
+  reverseFile?: File,
+  new?: boolean
 }
 
 // 获取本地缓存的列表
-const getLSFiles = () => {
-
+const getLSFiles = (): LocalFileInfo[]  => {
   const voiceFiles = JSON.parse(Taro.getStorageSync('voiceFiles') || '[]')
   return voiceFiles.sort((a: LocalFileInfo, b: LocalFileInfo) => a.index > b.index ? -1 : 1)
 }
 
 // 写入本地缓存的正常文件
 const setLSFile = (savedFilePath: string) => {
-  console.log('savedFilePath', 111)
   const voiceFiles = getLSFiles()
-  console.log(voiceFiles)
-  const index = voiceFiles.length > 0 ? voiceFiles[voiceFiles.length - 1].index + 1 : 1
+  const index = voiceFiles.length > 0 ? voiceFiles[0].index + 1 : 1
   const fileInfo = {
     path: savedFilePath,
     index,
+    reverseFilePath: '',
   }
 
-  voiceFiles.push(fileInfo)
+  voiceFiles.unshift(fileInfo)
 
   Taro.setStorageSync('voiceFiles', JSON.stringify(voiceFiles))
   return fileInfo
 }
 
 // 获取本地缓存列表
-export const getFiles = () => new Promise((resolve, reject) => {
+export const getFiles = (): Promise<LocalFileInfo[]> => new Promise((resolve, reject) => {
   Taro.getSavedFileList({
     success: (res: any) => {
       const localFiles = getLSFiles()
       const fileList = res.fileList || []
       const files = localFiles.map((localFile: LocalFileInfo) => {
-        fileList.forEach((file: File) => {
+        fileList.forEach((file: File, index: number) => {
           if (localFile.path === file.filePath) {
+            // 源文件
             localFile.file = file
+            fileList[index].valid = true
           }
           if (localFile.reverseFilePath === file.filePath) {
+            // 反转文件
             localFile.reverseFile = file
+            fileList[index].valid = true
           }
         })
         return localFile
       })
+
+
+      // 删除缓存中没有存数据的文件
+      fileList.forEach((file: any) => {
+        if (!file.valid) {
+          removeSavedFilePromise(file.filePath)
+        }
+      })
+
       resolve(files)
     },
   })
@@ -105,7 +117,7 @@ export const reverse = (reverseFile: LocalFileInfo): Promise<any> => {
           url: `${API_URL}/${path}`,
           success: (saveRes) => {
             // 更新文件列表
-            // this.getFiles()
+            // this.await ()
             Taro.saveFile({
               tempFilePath: saveRes.tempFilePath,
               success: (res) => {
@@ -135,16 +147,53 @@ export const saveFile = (tempFilePath: string): Promise<LocalFileInfo> => {
     Taro.saveFile({
       tempFilePath,
       success: (res) => {
-        console.log(res.savedFilePath)
         const fileInfo = setLSFile(res.savedFilePath)
         resolve(fileInfo)
         // res.savedFilePath
         // 更新文件列表
         // this.getFiles()
       },
-      fail: () => {
-        reject()
-      },
+      fail: reject,
     })
   })
+}
+
+const removeSavedFilePromise = (filePath: string) => new Promise(resolve => {
+  Taro.removeSavedFile({
+    filePath: filePath,
+    success: resolve,
+    fail: resolve,
+  })
+})
+
+// 删除本地文件 和 文件 map 数据
+export const deleteFile = async (file: LocalFileInfo) => {
+  if (!file.file || !file.reverseFile) {
+    return
+  }
+  Taro.showLoading({ title: '删除中...', mask: true })
+
+  try {
+    let files = await getLSFiles()
+    console.log(files)
+
+    await Promise.all([
+      removeSavedFilePromise(file.file.filePath),
+      removeSavedFilePromise(file.reverseFile.filePath),
+    ])
+    Taro.showToast({
+      title: '删除成功',
+      icon: 'success',
+    })
+
+    Taro.setStorageSync('voiceFiles', JSON.stringify(files.filter((f) => f.index !== file.index)))
+  } catch (error) {
+    Taro.showToast({
+      title: '删除失败',
+      icon: 'none',
+    })
+  }
+
+  Taro.hideLoading()
+
 }
