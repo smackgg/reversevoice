32e2f4d1c42ae1ff63f6ccb318679b66919e6292
smackgg <smackgg@gmail.com>
2019-11-19 21:10:08 +0800

    remove console log

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -13,14 +13,13 @@ import { getWxAuthorization, WXBizDataCrypt } from '../util/wechat'
 export const postLogin = async (ctx: any) => {
   const { code, iv, encryptedData } = ctx.request.fields
 
-  // const { code } = ctx.req.body
-  if (!code) ctx.throw(401, 'no jscode')
+  if (!code || !iv || !encryptedData) ctx.throw(401, 'no jscode')
   try {
     // 微信鉴权
     const data = await getWxAuthorization(code)
     const pc = new WXBizDataCrypt(data.session_key)
     const { openId, unionId, ...profile } = pc.decryptData(encryptedData, iv)
-    // console.log(wxData, 1111)
+
     // 更新数据库用户信息
     await User.findOneAndUpdate({ openid: data.openid }, {
       openid: openId,
@@ -33,7 +32,7 @@ export const postLogin = async (ctx: any) => {
     // 更新 session
     ctx.session.sessionKey = data.session_key
     ctx.session.openid = data.openid
-    // console.log(ctx.session, '----------')
+
     ctx.body = {
       code: 0,
       message: 'ok',
