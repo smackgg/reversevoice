20a23a35752244a056f6e5529cfd23205bd99269
smackgg <smackgg@gmail.com>
2019-11-20 20:58:12 +0800

    updatefile api

--- wechatapp/src/services/file.ts
+++ wechatapp/src/services/file.ts
@@ -2,7 +2,6 @@ import request from '../utils/request'
 
 // 检测登录token是否有效
 export const deleteFile = (data: { path: string }) => request({
-  url: '/api/file/mp3/reverse',
-  data,
+  url: `/api/file/mp3/reverse?path=${data.path}`,
   method: 'DELETE',
 })
