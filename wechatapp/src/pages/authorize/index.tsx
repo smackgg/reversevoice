8398a70f62cbae2720f4b5dff33e60c8a3e81e72
smackgg <smackgg@gmail.com>
2019-11-20 20:57:57 +0800

    remove useless code

--- wechatapp/src/pages/authorize/index.tsx
+++ wechatapp/src/pages/authorize/index.tsx
@@ -115,56 +115,6 @@ class Auth extends Component {
               return
             }
             this.handleLoginSuccess()
-            // Taro.hideLoading()
-            // this.login()
-          },
-        })
-        // // 去注册
-        // if (error && error.code == 10000) {
-        //   this.registerUser()
-        //   return
-        // }
-
-        // // 登录错误
-        // if (error || result.code !== 0) {
-        //   Taro.hideLoading()
-        //   Taro.showModal({
-        //     title: '提示',
-        //     content: '无法登录，请重试',
-        //     showCancel: false,
-        //   })
-        //   return
-        // }
-        // const { token, uid } = result.data
-        // Taro.setStorageSync('token', token)
-        // Taro.setStorageSync('uid', uid)
-
-        // this.handleLoginSuccess()
-      },
-    })
-  }
-
-  // 用户注册
-  registerUser = () => {
-    Taro.login({
-      success: res => {
-        // 微信登录接口返回的 code 参数，下面注册接口需要用到
-        const { code } = res
-        Taro.getUserInfo({
-          success: async result => {
-            const { iv, encryptedData } = result
-            // 推荐人
-            const referrer = Taro.getStorageSync('referrer') || ''
-            // 注册
-            await register({
-              code,
-              encryptedData,
-              iv,
-              referrer,
-            })
-
-            Taro.hideLoading()
-            this.login()
           },
         })
       },
