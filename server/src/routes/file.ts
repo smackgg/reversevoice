d52584470678f25546f523f90873da4fd2f7364c
smackgg <smackgg@gmail.com>
2019-11-20 20:55:39 +0800

    update router

--- server/src/routes/file.ts
+++ server/src/routes/file.ts
@@ -1,21 +1,14 @@
 import Router from 'koa-router'
-import { postMp3Reverse, deleteMp3Reverse } from '../controllers/file'
+import { postMp3Reverse, deleteMp3Reverse, postMp3Upload } from '../controllers/file'
 
 const router = new Router<null, { needWechatLogin(): void }>()
 
 router.prefix('/api/file')
-// async (ctx) => {
-//   await ctx.needWechatLogin()
-//   console.log(ctx)
-// }
+
 router.post('/mp3/reverse', async ctx => postMp3Reverse(ctx))
 
 router.delete('/mp3/reverse', async ctx => deleteMp3Reverse(ctx))
 
-
-
-// router.put('/userinfo', async (ctx) => updateUserInfo(ctx))
-
-// router.get('/userinfo', async (ctx) => getUserInfo(ctx))
+router.post('/mp3/upload', async ctx => postMp3Upload(ctx))
 
 module.exports = router
