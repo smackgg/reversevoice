69d82750c4b46773f467e039136aed4ff5e8060d
smackgg <smackgg@gmail.com>
2019-11-20 20:50:58 +0800

    add postFile api

--- server/src/controllers/file.ts
+++ server/src/controllers/file.ts
@@ -5,10 +5,10 @@ import ffmpeg from 'fluent-ffmpeg'
 import fs from 'fs'
 import dateFormat from '../util/dateFormat'
 import { cErr } from '../util'
+import { MEDIA_URL } from '../util/secrets'
 import Errors from '../errors'
+import upLoadFile from '../util/qiniu'
 
-// const ffprobePath = require('@ffprobe-installer/ffprobe').path;
-// const ffmpeg = require('fluent-ffmpeg')
 import path from 'path'
 
 ffmpeg.setFfmpegPath(ffprobePath)
@@ -20,16 +20,21 @@ type QiniuReply = {
 }
 
 // 反转视频
-const reverseVoice = (filepath: string): Promise<string> => new Promise((resolve, reject) => {
-  const folderPath = dateFormat(new Date(), 'YYYY_MM_dd') + '/'
+const reverseVoice = (filepath: string, fileName: string): Promise<string> => new Promise((resolve, reject) => {
+  const folderPath = dateFormat(new Date(), 'YYYY_MM_dd')
   const publicPath = path.resolve((__dirname + '../../public/' + folderPath))
   if (!fs.existsSync(publicPath)) {
     fs.mkdirSync(publicPath)
   }
   const saveFilePath = '/reverse_' + uuidv4() + '_reverse.mp3'
+  const match = filepath.match(/\.durationTime=(\d+)/)
+  // console.log(fileName)
+  const duration = Number(match && match[1] ? match[1] : 0)
 
   ffmpeg(filepath)
     .format('mp4')
+    // .duration(duration / 1000)
+
     .outputOptions([
       // '-c copy',
       '-vf reverse',
@@ -39,7 +44,6 @@ const reverseVoice = (filepath: string): Promise<string> => new Promise((resolve
       '-y',
       // '-shortest'
     ])
-
     .on('progress', (progress) => {
       console.log(`Processing: ${progress.percent}% done`)
 
@@ -52,8 +56,10 @@ const reverseVoice = (filepath: string): Promise<string> => new Promise((resolve
       console.log(`Ffmpeg has been killed${err.message}`)
     })
     .toFormat('mp3')
+    // .duration(duration / 1000)
     .save(publicPath + saveFilePath)
-    .on('end', () => {
+    .on('end', (res) => {
+      console.log(res, '1111')
       resolve(folderPath + saveFilePath)
     })
 })
@@ -63,11 +69,12 @@ const reverseVoice = (filepath: string): Promise<string> => new Promise((resolve
  */
 export const postMp3Reverse = async (ctx: any) => {
   const file = ctx.request.files[0]
+
   if (!file) {
     return ctx.throw(400, '请上传正确的文件')
   }
   // const reply = await reverseVoice(file.path)
-  const [err, saveFilePath] = await cErr(reverseVoice(file.path))
+  const [err, saveFilePath] = await cErr(reverseVoice(file.path, file.name))
   if (err) {
     ctx.throw(500, err)
     return
@@ -95,6 +102,31 @@ export const deleteMp3Reverse = async (ctx: any) => {
   fs.unlinkSync(path.resolve((__dirname + '../../public/' + filepath)))
 
   ctx.body = {
+    code: 0,
+  }
+}
+
+/**
+ * POST /api/mp3/upload
+ * 上传文件.
+ */
+
+export const postMp3Upload = async (ctx: any) => {
+  const file = ctx.request.files[0]
+  if (!file) {
+    return ctx.throw(400, '请上传正确的文件')
+  }
+  // const reply = await reverseVoice(file.path)
+  // const [err, saveFilePath] = await cErr(reverseVoice(file.path))
+  const [err, result] = await cErr(upLoadFile('reverse-voice', uuidv4() + '.mp3', file.path))
+  if (err || result.error) {
+    ctx.throw(500, err || result.error)
+    return
+  }
+  ctx.body = {
+    data: {
+      path: MEDIA_URL + result.key,
+    },
     code: 0,
   }
 }
